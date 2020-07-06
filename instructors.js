e85145d809ce2f95b020ed4273f88466755dd4b8
Charles Sal <charles.asal@outlook.com>
2020-07-06 20:47:35 -0300

    format date

--- instructors.js
+++ instructors.js
@@ -1,3 +1,26 @@
+const fs = require('fs')
+const data = require('./data.json')
+const { age } = require('./utils')
+
+exports.show = function(req, res) {
+  const { id } = req.params
+
+  const foundInstructor = data.instructors.find(function(instructor) {
+    return id == instructor.id
+  })
+
+  if (!foundInstructor) return res.send('Instructor not found!') 
+
+  const instructor = {
+    ...foundInstructor,
+    age: age(foundInstructor.birth),
+    services: foundInstructor.services.split(','),
+    created_at: new Intl.DateTimeFormat('pt-BR').format(foundInstructor.created_at),
+  }
+
+  return res.render('instructors/show', { instructor })
+}
+
 //create
 exports.post = function(req, res) {
   
@@ -8,6 +31,29 @@ exports.post = function(req, res) {
       return res.send('Please, fill all fields!')
   }
 
-  return res.send(req.body)
+  let { avatar_url, name, gender, birth, services } = req.body
+
+  birth = Date.parse(req.body.birth)
+  const created_at = Date.now()
+  const id = Number(data.instructors.length + 1)
+
+
+  data.instructors.push({
+    id,
+    avatar_url,
+    name,
+    birth,
+    gender,
+    services,
+    created_at
+  })
+
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+    if (err) return res.send('Write file error!')
+
+    return res.redirect('/instructors')
+  })
+
+  //return res.send(req.body)
   
 }
