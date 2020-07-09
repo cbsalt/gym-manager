4c8310dc97e147a0afa7231511ec2efdd5465609
Charles Sal <charles.asal@outlook.com>
2020-07-08 22:21:55 -0300

    date format

--- instructors.js
+++ instructors.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
 const data = require('./data.json')
-const { age } = require('./utils')
+const { age, date } = require('./utils')
 
+//show
 exports.show = function(req, res) {
   const { id } = req.params
 
@@ -53,7 +54,22 @@ exports.post = function(req, res) {
 
     return res.redirect('/instructors')
   })
-
-  //return res.send(req.body)
   
+}
+
+exports.edit = function(req, res) {
+  const { id } = req.params
+
+  const foundInstructor = data.instructors.find(function(instructor) {
+    return id == instructor.id
+  })
+
+  if (!foundInstructor) return res.send('Instructor not found!')
+
+  const instructor = {
+    ...foundInstructor,
+    birth: date(foundInstructor.birth)
+  }
+
+  return res.render('instructors/edit', { instructor })
 }
