e1228e3c6a2e7818e9c1e2028fb2bc8366e4671c
Charles Sal <charles.asal@outlook.com>
2020-08-03 22:42:13 -0300

    creating relationship between databases using postgres

--- src/app/models/Member.js
+++ src/app/models/Member.js
@@ -23,8 +23,8 @@ module.exports = {
       birth,
       blood,
       weight,
-      height
-    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+      height,instructor_id
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
     RETURNING id
     ` 
 
@@ -36,7 +36,8 @@ module.exports = {
       date(data.birth).iso,
       data.blood,
       data.weight,
-      data.height
+      data.height,
+      data.instructor
     ]
 
     db.query(query, values, function(err, results) {
@@ -49,9 +50,10 @@ module.exports = {
   },
   find(id, callback) {
     db.query(`
-      SELECT * 
-      FROM members 
-      WHERE id = $1`, [id], function(err, results) {
+      SELECT members.*, instructors.name AS instructor_name
+      FROM members
+      LEFT JOIN instructors ON (members.instructor_id = instructors.id)
+      WHERE members.id = $1`, [id], function(err, results) {
         if(err) throw `Database Error! ${err}`
 
         callback(results.rows[0])
@@ -67,8 +69,9 @@ module.exports = {
       email=($5),
       blood=($6),
       weight=($7),
-      height=($8)
-    WHERE id = $9
+      height=($8),
+      instructor_id=($9)
+    WHERE id = $10
     `
 
     const values = [
@@ -80,6 +83,7 @@ module.exports = {
       data.blood,
       data.weight,
       data.height,
+      data.instructor,
       data.id
     ]
 
@@ -94,6 +98,13 @@ module.exports = {
       if(err) throw `Database Error! ${err}`
 
       return callback()
+    })
+  },
+  instructorsSelectOptions(callback) {
+    db.query(`SELECT name, id FROM instructors`, function(err, results) {
+      if(err) throw `Database Error! ${err}`
+
+      callback(results.rows)
     })
   }
 }
